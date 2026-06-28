a07be39e0859c8279ccc9e9d7b755a8c72aa5361
scybudtech <scybudtech@gmail.com>
2026-06-28 21:30:52 +0200

    feat: Implement password reset functionality with email submission and new password handling

---
+++ js/auth/resetPassword.js
@@ -0,0 +1,94 @@
+import { supabase } from "../supabase.js";
+import { setButtonLoading } from "https://scybud.github.io/scybud-ui/js/ui.js";
+
+//PASSWORD RESET EMAIL FORM SUBMISSON FUNCTION
+async function sendPasswordResetEmail(email, redirectPage) {
+  const { error } = await supabase.auth.resetPasswordForEmail(email, {
+    redirectTo: redirectPage,
+  });
+
+  if (error) {
+    console.error(error);
+    alert(error.message);
+    return false;
+  }
+
+  const successCard = document.querySelector(".success-card");
+  const passwordResetEmailForm = document.getElementById(
+    "passwordResetEmailForm",
+  );
+
+  passwordResetEmailForm.classList.add("hide");
+  successCard.classList.add("showFlex");
+
+  return true;
+}
+
+//PASSWORD RESET EMAIL FORM SUBMISSON
+const passwordResetEmailForm = document.getElementById(
+  "passwordResetEmailForm",
+);
+
+if (passwordResetEmailForm) {
+  passwordResetEmailForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("passwordResetEmail").value.trim();
+
+    const button = document.getElementById("passwordResetEmailSubmitBtn");
+    setButtonLoading(button, true);
+
+    try {
+      const success = await sendPasswordResetEmail(
+        email,
+        "https://joinpropdek.vercel.app/auth/changePassword",
+      );
+
+      if (!success) {
+        return;
+      }
+    } finally {
+      setButtonLoading(button, false);
+      document.getElementById("passwordResetEmail").value = "";
+    }
+  });
+}
+
+//PASSWORD CHANGE NEW PASSWORD FORM SUBMISSON FUNCTION
+async function sendPasswordResetNewPassword(password) {
+  const { error } = await supabase.auth.updateUser({
+    password: password,
+  });
+
+  if (error) {
+    console.error(error);
+    alert(error.message);
+    return false;
+  }
+
+  alert("Password reset Complete!");
+  window.location.href = "https://app.loghue.com";
+  return true;
+}
+
+//PASSWORD RESET EMAIL FORM SUBMISSON
+const passwordChangeForm = document.getElementById("passwordChangeForm");
+
+if (passwordChangeForm) {
+  passwordChangeForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const password = document
+      .getElementById("passwordResetNewPassword")
+      .value.trim();
+
+    const button = document.getElementById("passwordResetNewPasswordSubmitBtn");
+    setButtonLoading(button, true);
+
+    try {
+      await sendPasswordResetNewPassword(password);
+    } finally {
+      setButtonLoading(button, false);
+    }
+  });
+}
